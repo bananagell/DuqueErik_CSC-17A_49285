bad5a66dfe2adb5d006b5f4ff126674733222dad
ed2277011 <eduque791@gmail.com>
2020-09-03 14:13:47 -0700

    Signed-off-by: ed2277011 <eduque791@gmail.com>

---
+++ Lab/1_IntroLab_Hello_World/Hello World/Hello_World.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   Hello_Wold.cpp
+ * Author: Erik Duque
+ * Created on September 1st, 2020, 9:50 PM
+ * Lab:  Intro Lab Hello World
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   std::cout << "Hello World!";
+    return 0;
+}
